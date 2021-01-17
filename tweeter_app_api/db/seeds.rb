3bdc601b1651fe893a61547ed701d3a62634721f
Anita Jackson <anitajackson@Anitas-MacBook-Pro.local>
2021-01-17 13:44:19 -0500

    added Faker to the application

--- tweeter_app_api/db/seeds.rb
+++ tweeter_app_api/db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do
+    Tweet.create({
+        title: Faker::Twitter.user(include_status: false),
+        content: Faker::Twitter.status,
+        author: Faker::Twitter.screen_name
+        
+    })
+end
+
+# Tweet.create({
+#     title: "Just found this",
+#     content: "the square of the hypotenuse is equal to the sum of the squares of the other two sides",
+#     author: "Pythagoras570"
+#   })
+  
+#   Tweet.create({
+#     title: "I'm walkin' here",
+#     content: "Hey, I'm walkin' here!",
+#     author: "Nicky62"
+#   })
